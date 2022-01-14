46a7582f55d5d541a04c6d061282c5519920684f
printpal.io <95444610+printpal-io@users.noreply.github.com>
2022-01-13 19:16:31 -0600

    Add files via upload

---
+++ OctoPrint-Printwatch/octoprint_printwatch/static/js/printwatch.js
@@ -0,0 +1,30 @@
+$(function() {
+    function PrintwatchViewModel(parameters) {
+        var self = this;
+
+        self.settings = parameters[0];
+
+
+        // this will be called when the user clicks the "Go" button and set the iframe's URL to
+        // the entered URL
+
+
+        // This will get called before the PrintwatchViewModel gets bound to the DOM, but after its
+        // dependencies have already been initialized. It is especially guaranteed that this method
+        // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
+        // the SettingsViewModel been properly populated.
+    }
+
+    // This is how our plugin registers itself with the application, by adding some configuration
+    // information to the global variable OCTOPRINT_VIEWMODELS
+    OCTOPRINT_VIEWMODELS.push([
+        // This is the constructor to call for instantiating the plugin
+        PrintwatchViewModel,
+
+        // This is a list of dependencies to inject into the plugin, the order which you request
+        // here is the order in which the dependencies will be injected into your view model upon
+        // instantiation via the parameters argument
+        ["settingsViewModel"],
+
+    ]);
+});
