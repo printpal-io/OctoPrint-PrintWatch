fab859d442bce1d8db6fe0c4291fa97108fe9e41
printpal.io <95444610+printpal-io@users.noreply.github.com>
2022-01-13 20:46:50 -0600

    Update __init__.py

--- octoprint_printwatch/__init__.py
+++ octoprint_printwatch/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 from octoprint.events import Events
-from .utils.videostreamer import VideoStreamer
-from .utils.comm import CommManager
-from .utils.inferencer import Inferencer
-from .utils.printer import PrinterControl
+from .videostreamer import VideoStreamer
+from .comm import CommManager
+from .inferencer import Inferencer
+from .printer import PrinterControl
 import os
 
 class PrintWatchPlugin(octoprint.plugin.StartupPlugin,
