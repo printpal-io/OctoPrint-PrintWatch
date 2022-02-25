a47a4fbbb1cdb70c711b3e102dfed72014d54159
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2022-02-24 22:58:43 -0600

    Update inferencer.py

--- octoprint_printwatch/inferencer.py
+++ octoprint_printwatch/inferencer.py
@@ -34,10 +34,8 @@ class Inferencer():
                     if self.plugin.streamer.jpg is not None:
                         self.plugin.comm_manager.send_request()
                         self._buffer_check()
-                        self.plugin._logger.info("After buffer check... Length of buffer: {}".format(len(self.circular_buffer)))
                         if len(self.circular_buffer) == int(self.plugin._settings.get(["buffer_length"])):
                             self.current_percent = [i[0] for i in self.circular_buffer].count(True) / int(self.plugin._settings.get(["buffer_length"]))
-                            self.plugin._logger.info("Current % calculated... Curr Perc: {}".format(self.current_percent))
                             if self.current_percent >= int(self.plugin._settings.get(["buffer_percent"])) / 100.0:
                                 pause_condition = (not self.triggered or (not self.plugin._printer.is_pausing() and self.plugin._printer.is_printing())) and self.plugin._settings.get(["enable_shutoff"])
                                 if pause_condition:
