5b47caabcc72d1678e913d7a6ca040e5694b8787
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2022-07-25 21:10:22 -0500

    Add files via upload

---
+++ comm.py
@@ -0,0 +1,226 @@
+import octoprint.plugin
+from urllib.request import Request, urlopen
+from socket import gethostbyname, gethostname
+from threading import Thread
+from time import time, sleep
+from datetime import datetime
+from threading import Lock
+from json import loads, dumps
+from base64 import b64encode
+from uuid import uuid4
+import io
+import PIL.Image as Image
+from PIL import ImageDraw
+import re
+
+
+DEFAULT_ROUTE = 'https://printwatch-printpal.pythonanywhere.com'
+
+class CommManager(octoprint.plugin.SettingsPlugin):
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.heartbeat_interval = 30.0
+        self.parameters = {
+                            'ticket' : '',
+                            'last_t' : 0.0,
+                            'ip' : gethostbyname(gethostname()),
+                            'route' : DEFAULT_ROUTE,
+                            'id' : self.plugin._settings.global_get(["accessControl", "salt"]) if self.plugin._settings.global_get(["accessControl", "salt"]) is not None else uuid4().hex,
+                            'bad_responses' : 0,
+                            'notification' : ''
+                            }
+
+
+    def _heartbeat(self):
+        while self.plugin._settings.get(["enable_detector"]) and self.heartbeat:
+            sleep(1.0) #prevent cpu overload
+            if time() - self.parameters['last_t'] > self.heartbeat_interval:
+                try:
+                    response = self._send('heartbeat')
+                    self._check_action(response)
+                except Exception as e:
+                    self.plugin._logger.info(
+                        "Error with Heartbeat: {}".format(str(e))
+                    )
+
+                self.parameters['last_t'] = time()
+        self.plugin._logger.info("Heartbeat loop closed")
+
+
+
+    def _create_payload(self, image=None):
+        return dumps({
+                    'image_array' : image,
+                    'settings' : {
+                        'email_addr' : self.plugin._settings.get(["email_addr"]),
+                        'notification_threshold' : self.plugin._settings.get(["notification_threshold"]),
+                        'action_threshold' : self.plugin._settings.get(["action_threshold"]),
+                        'confidence' : self.plugin._settings.get(["confidence"]),
+                        'buffer_length' : self.plugin._settings.get(["buffer_length"]),
+                        'buffer_percent' : self.plugin._settings.get(["buffer_percent"]),
+                        'api_key' : self.plugin._settings.get(["api_key"]),
+                        'enable_feedback_images' : self.plugin._settings.get(["enable_feedback_images"])
+                    },
+                    'parameters' : self.parameters,
+                    'job' : self.plugin._printer.get_current_job(),
+                    'data' : self.plugin._printer.get_current_data(),
+                    'state' : self.plugin._printer.get_state_id(),
+                    'version' : self.plugin._plugin_version,
+                    'scores' : self.plugin.inferencer.scores
+                }).encode('utf8')
+
+
+    def _send(self, endpoint='inference'):
+        data = self._create_payload() if endpoint =='heartbeat' else self._create_payload(b64encode(self.image).decode('utf8'))
+
+        inference_request = Request(
+            '{}/{}/'.format(self.parameters['route'], endpoint),
+            data=data,
+            method='POST',
+            headers={'User-Agent': 'Mozilla/5.0'}
+        )
+
+        return loads(urlopen(inference_request, timeout=10).read())
+
+    def _check_action(self, response):
+        if response['actionType'] == 'pause':
+            while not ((self.plugin._printer.is_pausing() and self.plugin._printer.is_printing()) or self.plugin._printer.is_paused()):
+                self.plugin._printer.pause_print()
+        elif response['actionType'] == 'stop':
+            while not (self.plugin._printer.is_cancelling() and self.plugin._printer.is_printing()):
+                self.plugin._printer.cancel_print()
+        elif response['actionType'] == 'resume':
+            if self.plugin._printer.is_paused():
+                while not self.plugin._printer.is_printing():
+                    self.plugin._printer.resume_print()
+
+    def _create_ticket(self):
+        ticket = uuid4().hex
+        self.parameters['ticket'] = ticket
+        self.plugin._logger.info(
+            "Ticket {} created for print job".format(ticket)
+        )
+
+    def _appends(self, response):
+        self.plugin.inferencer.circular_buffer.append([eval(response['defect_detected']), time()])
+        self.plugin.inferencer.scores.append(response['score'])
+        self.plugin.inferencer.action_level = response['levels']
+        self.plugin.inferencer.smas.append(response['smas'][0])
+
+    def start_service(self):
+        self.heartbeat = True
+        if self.plugin._settings.get(["enable_detector"]):
+            if self.plugin.inferencer.inference_loop is None:
+                self.heartbeat_loop = Thread(target=self._heartbeat)
+                self.heartbeat_loop.daemon = True
+                self.heartbeat_loop.start()
+                self.plugin._logger.info("PrintWatch heartbeat service started")
+
+    def kill_service(self):
+        self.heartbeat = False
+        self.heartbeat_loop = None
+        self.plugin._logger.info("PrintWatch heartbeat service terminated")
+
+    def send_request(self):
+        self.image = self.plugin.streamer.grab_frame()
+        try:
+            response = self._send()
+            self.parameters['last_t'] = time()
+            if response['statusCode'] == 200:
+                self._appends(response)
+                self._check_action(response)
+                self.parameters['bad_responses'] = 0
+                self.plugin.inferencer.REQUEST_INTERVAL = 10.0
+                boxes = eval(re.sub('\s+', ',', re.sub('\s+\]', ']', re.sub('\[\s+', '[', response['boxes'].replace('\n','')))))
+                self.plugin._plugin_manager.send_plugin_message(
+                    self.plugin._identifier,
+                    dict(
+                        type="display_frame",
+                        image=self.draw_boxes(boxes)
+                    )
+                )
+                self.plugin._plugin_manager.send_plugin_message(
+                    self.plugin._identifier,
+                    dict(
+                        type="icon",
+                        icon='plugin/printwatch/static/img/printwatch-green.gif'
+                    )
+                )
+            elif response['statusCode'] == 213:
+                self.plugin.inferencer.REQUEST_INTERVAL= 300.0
+            else:
+                self.plugin.inferencer.pred = False
+                self.parameters['bad_responses'] += 1
+                self.plugin.inferencer.REQUEST_INTERVAL = 10.0
+                self.plugin._logger.info(
+                    "Payload: {} {}".format(
+                        self.plugin._settings.get([]),
+                        self.parameters
+                    )
+                )
+                self.plugin._logger.info(
+                    "Response: {}".format(response)
+                )
+
+        except Exception as e:
+            self.plugin._logger.info(
+                "Error retrieving server response: {}".format(str(e))
+            )
+            self.parameters['bad_responses'] += 1
+            self.plugin.inferencer.pred = False
+            self.parameters['last_t'] = time()
+
+    def draw_boxes(self, boxes):
+        pil_img = Image.open(io.BytesIO(self.image))
+        process_image = ImageDraw.Draw(pil_img)
+        width, height = pil_img.size
+
+        for i, det in enumerate(boxes):
+            det = [j / 640 for j in det]
+            x1 = (det[0] - (det[2]/2))*width
+            y1 = (det[1] - (det[3]/2))* height
+            x2 = (det[0] + (det[2]/2))*width
+            y2 = (det[1] + (det[3]/2))*height
+            process_image.rectangle([(x1, y1), (x2, y2)], fill=None, outline="red", width=4)
+
+        out_img = io.BytesIO()
+        pil_img.save(out_img, format='PNG')
+        contents = b64encode(out_img.getvalue()).decode('utf8')
+        return 'data:image/png;charset=utf-8;base64,' + contents.split('\n')[0]
+
+    def email_notification(self, notification_level):
+        if self.plugin._settings.get(["enable_email_notification"]):
+            try:
+                self.parameters['notification'] = notification_level
+                self.parameters['time'] = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                response = self._send('notify')
+                self.plugin._logger.info(
+                    "Notification sent to {}".format(self.plugin._settings.get(["email_addr"]))
+                )
+            except Exception as e:
+                self.plugin._logger.info(
+                    "Error retrieving server response for email notification: {}".format(str(e))
+                )
+
+    def send_feedback(self, classification):
+        self.parameters['feedback'] = {
+            'feedback' : True,
+            'time' : time(),
+            'class' : classification
+        }
+        try:
+            self.image = self.plugin.streamer.grab_frame()
+            response = self._send('feedback')
+            self.plugin._logger.info(
+                "Feedback sent for defect identified as: {}".format(classification)
+            )
+        except Exception as e:
+            self.plugin._logger.info(
+                "Error retrieving server response for feedback sending: {}".format(str(e))
+            )
+        return
+
+
+
+    def new_ticket(self):
+        self._create_ticket()
---
+++ inferencer.py
@@ -0,0 +1,132 @@
+from threading import Thread
+from time import time, sleep
+
+MIN_MULTIPLIER = 0.5
+MAX_MULTIPLIER = 4
+
+class Inferencer():
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.circular_buffer = []
+        self.scores = []
+        self.current_percent = 0.0
+        self.triggered = False
+        self.warning_notification = False
+        self.pred = False
+        self.REQUEST_INTERVAL = 10.0
+        self.inference_loop = None
+        self.action_level = []
+        self.cooldown_time = 0.0
+        self.smas = []
+
+    def _buffer_check(self):
+        buffer_length = int(self.plugin._settings.get(["buffer_length"]))
+        if len(self.smas) > 0:
+            self.plugin._plugin_manager.send_plugin_message(
+                self.plugin._identifier,
+                dict(
+                    type="score",
+                    scores=self.smas[-1],
+                    pop=len(self.scores) > int(buffer_length * MAX_MULTIPLIER)
+                )
+            )
+        while len(self.circular_buffer) > buffer_length:
+            self.circular_buffer.pop(0)
+        while len(self.scores) > int(buffer_length * MAX_MULTIPLIER):
+            self.scores.pop(0)
+            self.smas.pop(0)
+
+        self.current_percent = [i[0] for i in self.circular_buffer].count(True) / buffer_length if len(self.circular_buffer) == buffer_length else 0.0
+        self.action_level = self.action_level if len(self.scores) >= buffer_length else [False, False, False]
+        self._action_check()
+
+    def _action_check(self):
+        self.warning_notification = time() - self.cooldown_time < 600 and self.cooldown_time > 0.0 if self.cooldown_time > 0.0 else self.warning_notification #10 minute cooldown
+        if self.action_level[1]:
+            #pause/stop the print
+            if not self.triggered:
+                self.plugin._logger.info("Action level reached")
+                if self.plugin._settings.get(["enable_stop"]):
+                    self._attempt_action('cancel')
+                    self.triggered = True
+                elif self.plugin._settings.get(["enable_shutoff"]):
+                    self._attempt_action('pause')
+                    self.triggered = True
+        elif self.action_level[0]:
+            if not self.warning_notification:
+                self.plugin._logger.info("Notification level reached")
+                if self.plugin._settings.get(["enable_email_notification"]):
+                    self.notification_event('warning')
+                    self.warning_notification = True
+                    self.cooldown_time = 0.0
+
+    def _attempt_action(self, action):
+        if action == 'cancel':
+            self.plugin._printer.cancel_print()
+        else:
+            self.plugin._printer.pause_print()
+        self.plugin._logger.info(
+            "Print {} command sent to OctoPrint.".format(action)
+        )
+
+
+    def _inferencing(self):
+        self.plugin._logger.info("PrintWatch Inference Loop starting...")
+        while self.run_thread and self.plugin._settings.get(["enable_detector"]):
+            sleep(0.1) #prevent cpu overload
+            if self.plugin._printer.is_printing() and not self.triggered:
+                if time() - self.plugin.comm_manager.parameters['last_t'] > self.REQUEST_INTERVAL:
+                    self.plugin.comm_manager.send_request()
+                    self._buffer_check()
+
+                if self.plugin.comm_manager.parameters['bad_responses'] >= int(self.plugin._settings.get(["buffer_length"])):
+                    self.plugin._logger.info("Too many bad response from server. Disabling PrintWatch monitoring")
+                    self.kill_service()
+
+    def start_service(self):
+        self.triggered = False
+        self.warning_notification = False
+        self.plugin.comm_manager.parameters['notification'] =  ''
+        if self.plugin._settings.get(["enable_detector"]):
+            if self.inference_loop is None:
+                self.run_thread = True
+                self.inference_loop = Thread(target=self._inferencing)
+                self.inference_loop.daemon = True
+                self.inference_loop.start()
+                self.plugin._logger.info("PrintWatch inference service started")
+                self.plugin._plugin_manager.send_plugin_message(
+                    self.plugin._identifier,
+                    dict(
+                        type="icon",
+                        icon='plugin/printwatch/static/img/printwatch-green.gif'
+                    )
+                )
+
+    def kill_service(self):
+        self.run_thread = False
+        self.inference_loop = None
+        self.REQUEST_INTERVAL = 10.0
+        self.plugin.comm_manager.parameters['bad_responses'] = 0
+        self.circular_buffer = []
+        self.action_level = []
+        self.scores = []
+        self.current_percent = 0.0
+        self.plugin._logger.info("PrintWatch inference service terminated")
+        self.plugin._plugin_manager.send_plugin_message(
+            self.plugin._identifier,
+            dict(
+                type="icon",
+                icon='plugin/printwatch/static/img/printwatch-grey.png'
+            )
+        )
+
+    def shutoff_event(self):
+        self.plugin.controller.shutoff_actions(self.plugin._settings.get(["enable_extruder_shutoff"]))
+        if self.triggered:
+            self.notification_event('action')
+
+    def notification_event(self, notification_level):
+        self.plugin.comm_manager.email_notification(notification_level)
+
+    def begin_cooldown(self):
+        self.cooldown_time = time()
