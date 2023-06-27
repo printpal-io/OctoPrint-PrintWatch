b0f45de3b2f4396d118e6e3059e4e25854eb3a57
printpal.io <95444610+printpal-io@users.noreply.github.com>
2023-06-27 11:01:54 -0500

    Add files via upload

--- octoprint_printwatch/__init__.py
+++ octoprint_printwatch/__init__.py
@@ -6,6 +6,7 @@ from .comm import CommManager
 from .inferencer import Inferencer
 from .printer import PrinterControl
 import asyncio
+import logging
 
 class PrintWatchPlugin(octoprint.plugin.StartupPlugin,
                            octoprint.plugin.ShutdownPlugin,
@@ -18,7 +19,7 @@ class PrintWatchPlugin(octoprint.plugin.StartupPlugin,
 
 
     def __init__(self):
-        self._logger.info("Loading PrintWatch...")
+        self._logger = logging.getLogger('octoprint.plugins.printwatch')
         self.streamer = VideoStreamer(self)
         self.inferencer = Inferencer(self)
         self.comm_manager = CommManager(self)
@@ -26,6 +27,7 @@ class PrintWatchPlugin(octoprint.plugin.StartupPlugin,
 
 
     def on_after_startup(self):
+        self._logger.info("Loading PrintWatch...")
         self.comm_manager.start_service()
 
 
