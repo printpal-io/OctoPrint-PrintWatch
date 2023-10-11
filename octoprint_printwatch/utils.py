45b72bd27c62904e6ed901d88b7ecfebc1737f91
printpal.io <95444610+printpal-io@users.noreply.github.com>
2023-10-11 12:59:55 -0500

    Add files via upload

--- octoprint_printwatch/utils.py
+++ octoprint_printwatch/utils.py
@@ -27,34 +27,34 @@ def ps_util_get_stats() -> dict:
         response_ = {}
         thermal = psutil.sensors_temperatures(fahrenheit=False)
         if "cpu-thermal" in thermal:  # RPi
-            response_["cpu_temp"] = int(round((thermal["cpu-thermal"][0][1])))
+            response_["cpu_temp"] = float((thermal["cpu-thermal"][0][1]))
         elif "cpu_thermal" in thermal:  # RPi Alternative
-            response_["cpu_temp"] = int(round((thermal["cpu_thermal"][0][1])))
+            response_["cpu_temp"] = float((thermal["cpu_thermal"][0][1]))
         elif 'soc_thermal' in thermal:  # BananaPi
-            response_["cpu_temp"] = int(round(float(thermal['soc_thermal'][0][1])*1000))
+            response_["cpu_temp"] = float(thermal['soc_thermal'][0][1])*1000
         elif 'coretemp' in thermal:  # Intel
             for temp in range(0, len(thermal["coretemp"]), 1):
                 temp_sum = temp_sum+thermal["coretemp"][temp][1]
-            response_["cpu_temp"] = int(round(temp_sum / len(thermal["coretemp"])))
+            response_["cpu_temp"] = float(temp_sum / len(thermal["coretemp"]))
         elif 'w1_slave_temp' in thermal:  # Dallas temp sensor fix
             with open('/sys/class/thermal/thermal_zone0/temp') as temp_file:
                 cpu_val = temp_file.read()
-                response_["cpu_temp"] = int(round(float(cpu_val)/1000))
+                response_["cpu_temp"] = float(cpu_val)/1000
         elif "cpu" in thermal:  # RockPi (probably all RockChip CPUs/SOCs)
-            response_["cpu_temp"] = int(round((thermal["cpu"][0][1])))
+            response_["cpu_temp"] = float((thermal["cpu"][0][1]))
         elif 'cpu_thermal_zone' in thermal: #OrangePI_Zero2
-            response_["cpu_temp"] = int(round((thermal["cpu_thermal_zone"][0][1])))
+            response_["cpu_temp"] = float((thermal["cpu_thermal_zone"][0][1]))
         elif "scpi_sensors" in thermal: # Le Potato sbc
-            response_["cpu_temp"] = int(round((thermal["scpi_sensors"][0][1])))
+            response_["cpu_temp"] = float((thermal["scpi_sensors"][0][1]))
         elif "sunxi-therm-1" in thermal: # Orange Pi Zero Plus 2 H3
-            response_["cpu_temp"] = int(round(thermal["sunxi-therm-1"][0][1] * 1000))
+            response_["cpu_temp"] = float(thermal["sunxi-therm-1"][0][1] * 1000)
             if "sunxi-therm-2" in thermal:
                 # ok, get max temp
-                response_["cpu_temp"] = max([int(round(thermal["sunxi-therm-2"][0][1] * 1000)), response_["cpu_temp"]])
-        response_["cpu_percent"] = str(psutil.cpu_percent(interval=None, percpu=False))
-        response_["cpu_freq"] = str(int(round(psutil.cpu_freq(percpu=False), {})))
-        response_["virtual_memory_percent"] = str(psutil.virtual_memory().percent)
-        response_["disk_usage"] = str(psutil.disk_usage("/").percent)
+                response_["cpu_temp"] = max([float(thermal["sunxi-therm-2"][0][1] * 1000), response_["cpu_temp"]])
+        response_["cpu_percent"] = float(psutil.cpu_percent(interval=None, percpu=False))
+        response_["cpu_freq"] = float(round(psutil.cpu_freq(percpu=False), {}))
+        response_["virtual_memory_percent"] = float(psutil.virtual_memory().percent)
+        response_["disk_usage"] = float(psutil.disk_usage("/").percent)
 
         return response_
 
